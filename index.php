a483ea7fde00c1c3e57868292983b3357fca82d2
Doniel Acosta <donielacosta1995@gmail.com>
2022-05-04 16:50:30 -0400

    accesp

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HTML desde PHP</title>
+</head>
+<body>
+  <h1> ¡Hola, esto es HTML desde PHP!</h1>
+
+  <?php echo "Ahora esto se esta imprimiendo";?>
+</body>
+</html>
